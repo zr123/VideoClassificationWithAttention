9847677f87f5c4ee951e4854fe3151f2549df236
zr123 <25320035+zr123@users.noreply.github.com>
2021-08-31 22:25:59 +0200

    Attention Gate Grid Attention implementiert

--- AttentionGate.py
+++ AttentionGate.py
@@ -1,18 +1,17 @@
 import tensorflow as tf
 from tensorflow import keras
-import tensorflow.keras.backend as K
 from tensorflow.keras.layers import ReLU, Conv1D, Dense, Flatten, Reshape
 from tensorflow.keras.activations import softmax, sigmoid
 
 
-
 class AttentionGate(keras.layers.Layer):
 
-    def __init__(self, inter_channels=1, attention_function="pseudo-softmax"):
+    def __init__(self, inter_channels=1, attention_function="pseudo-softmax", grid_attention=False):
         super(AttentionGate, self).__init__()
         assert attention_function in ["softmax", "sigmoid", "pseudo-softmax"], "Unexpected attention_function argument."
         self.inter_channels = inter_channels
         self.attention_function = attention_function
+        self.grid_attention = grid_attention
 
     def call(self, inputs, **kwargs):
         local_features, global_features = inputs
@@ -41,7 +40,10 @@ class AttentionGate(keras.layers.Layer):
     def build(self, input_shape):
         local_feature_shape, global_feature_shape = input_shape
         self.local_Conv1D = Conv1D(self.inter_channels, 1, use_bias=False)
-        self.global_Dense  = Dense(self.inter_channels)
+        if self.grid_attention:
+            self.global_transformation = Conv1D(self.inter_channels, 1)
+        else:
+            self.global_transformation = Dense(self.inter_channels)
         self.joining_Conv1D = Conv1D(self.inter_channels, 1)
 
     def get_config(self):
@@ -56,8 +58,13 @@ class AttentionGate(keras.layers.Layer):
 
     def compatibility_function(self, local_features, global_features):
         local_component = self.local_Conv1D(local_features)
-        global_component = self.global_Dense(global_features)
+        if self.grid_attention:
+            row = int(local_features.shape[1] / global_features.shape[1])
+            col = int(local_features.shape[2] / global_features.shape[2])
+            global_features = tf.keras.layers.UpSampling2D((row, col), interpolation='bilinear')(global_features)
+        global_component = self.global_transformation(global_features)
         # expand g from (batch_size, inter_channels) to (batch_size, local_width, local_height, inter_channels)
-        expanded_g = tf.expand_dims(tf.expand_dims(global_component, axis=1), axis=1)
-        compatibility = self.joining_Conv1D(ReLU()(local_component + expanded_g))
+        if not self.grid_attention:
+            global_component = tf.expand_dims(tf.expand_dims(global_component, axis=1), axis=1)
+        compatibility = self.joining_Conv1D(ReLU()(local_component + global_component))
         return compatibility
--- AttentionModels.py
+++ AttentionModels.py
@@ -57,7 +57,7 @@ def get_attention_extractor(model):
 
 
 def create_AttentionGated_ResNet50v2(input_shape=(HEIGHT, WIDTH, CHANNELS), num_classes=CLASSES):
-    basenet = tf.keras.applications.ResNet50V2(classes=10, weights=None)
+    basenet = tf.keras.applications.ResNet50V2(input_shape=input_shape, classes=num_classes, weights=None)
 
     input_layer = basenet.input
 
@@ -75,6 +75,47 @@ def create_AttentionGated_ResNet50v2(input_shape=(HEIGHT, WIDTH, CHANNELS), num_
     a2 = AttentionGate(64)([local2.output, global_features.output])
     a3 = AttentionGate(64)([local3.output, global_features.output])
 
+
+    attended_features1 = local1.output * a1
+    attended_features1 = tf.keras.layers.GlobalAveragePooling2D()(attended_features1)
+    attended_features1 = Dense(2048, activation="relu")(attended_features1)
+    attended_features1 = Dense(10, activation="softmax")(attended_features1)
+
+    attended_features2 = local2.output * a2
+    attended_features2 = tf.keras.layers.GlobalAveragePooling2D()(attended_features2)
+    attended_features2 = Dense(2048, activation="relu")(attended_features2)
+    attended_features2 = Dense(10, activation="softmax")(attended_features2)
+
+    attended_features3 = local3.output * a3
+    attended_features3 = tf.keras.layers.GlobalAveragePooling2D()(attended_features3)
+    attended_features3 = Dense(2048, activation="relu")(attended_features3)
+    attended_features3 = Dense(10, activation="softmax")(attended_features3)
+
+    final = (attended_features1 + attended_features2 + attended_features3 + basenet.output) / 4.0
+
+    model = tf.keras.models.Model(inputs=input_layer, outputs=final)
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+    return model
+
+
+def create_AttentionGatedGrid_ResNet50v2(input_shape=(HEIGHT, WIDTH, CHANNELS), num_classes=CLASSES):
+    basenet = tf.keras.applications.ResNet50V2(input_shape=input_shape, classes=num_classes, weights=None)
+    input_layer = basenet.input
+
+    for layer in basenet.layers:
+        if layer.name == "conv2_block3_out":
+            local1 = layer
+        if layer.name == "conv3_block4_out":
+            local2 = layer
+        if layer.name == "conv4_block6_out":
+            local3 = layer
+
+    global_features = basenet.layers[-3]
+
+    a1 = AttentionGate(64, grid_attention=True)([local1.output, global_features.output])
+    a2 = AttentionGate(64, grid_attention=True)([local2.output, global_features.output])
+    a3 = AttentionGate(64, grid_attention=True)([local3.output, global_features.output])
 
     attended_features1 = local1.output * a1
     attended_features1 = tf.keras.layers.GlobalAveragePooling2D()(attended_features1)
